9ba380901446588bf492f9fc46b3e0d535906d6c
vagrant <alungm1@gmail.com>
2023-01-17 20:15:47 +0000

    modify

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,45 +1,32 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+listint_t *insert_node(listint_t **head, const int n)
 {
-	listint_t *head;
+	listint_t *temp = *head;
+	listint_t *new;
 
-	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-	printf("-----------------\n");
-
-	insert_node(&head, 27);
-
-	print_listint(head);
-
-	printf("-----------------\n");
-
-	insert_node(&head, -4);
-
-	print_listint(head);
-	printf("-----------------\n");
-
-	insert_node(&head, 1034);
-
-	print_listint(head);
-	free_listint(head);
-
-	return (0);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		if (temp->n >= n)
+		{
+			new->next = temp;
+			*head = new;
+			return (new);
+		}
+		while (temp->next && temp->next->n < n)
+			temp = temp->next;
+		new->next = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
